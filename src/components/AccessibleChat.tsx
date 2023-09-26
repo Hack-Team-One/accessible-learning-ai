c4f1f6c3420c38d37e442e0b6f4de56408873e9b
burtrico <burtrico@gmail.com>
2023-09-25 20:07:35 -0500

    updated textState to store objects

--- src/components/AccessibleChat.tsx
+++ src/components/AccessibleChat.tsx
@@ -3,9 +3,9 @@ import { useRecoilState } from 'recoil';
 import { Button, TextareaAutosize } from '@mui/base';
 import TextInput from '../components/TextInput';
 import {
-  textColorPrimaryState,
-  textFontPrimaryState,
-  textSizePrimaryState,
+  textColorState,
+  textFontState,
+  textSizeState,
 } from '../states/textState';
 import { bgColorState, borderColorState } from '../states/colorState';
 import { sendMessageToChatGPT } from '../api/chatGPT';
@@ -17,9 +17,9 @@ const CHATGPT_MODEL = process.env.NEXT_PUBLIC_CHATGPT_MODEL || 'gpt-3.5-turbo';
 const AccessibleChat: React.FC = () => {
   const [bgColor, setBgColor] = useRecoilState(bgColorState);
   const [borderColor, setBorderColor] = useRecoilState(borderColorState);
-  const [textColorPrimary, setTextColorPrimary] = useRecoilState(textColorPrimaryState);
-  const [textFontPrimary, setTextFontPrimary] = useRecoilState(textFontPrimaryState);
-  const [textSizePrimary, setTextSizePrimary] = useRecoilState(textSizePrimaryState);
+  const [textColor, setTextColor] = useRecoilState(textColorState);
+  const [textFont, setTextFont] = useRecoilState(textFontState);
+  const [textSize, setTextSize] = useRecoilState(textSizeState);
 
   const [userInput, setUserInput] = useState('');
   const [chatGPTResponse, setChatGPTResponse] = useState('');
@@ -46,10 +46,10 @@ const AccessibleChat: React.FC = () => {
         <TextareaAutosize 
           value={chatGPTResponse}
           onChange={(e) => setChatGPTResponse(e.target.value)}
-          className={`text-${textColorPrimary} w-full`}
+          className={`text-${textColor.primary} w-full`}
         />
         {chatGPTResponse.length > 0 && <Button
-          className={`text-${textColorPrimary} bg-${bgColor.primary} border border-${borderColor.primary} float-right button-regenerate rounded-md p-1 `}
+          className={`text-${textColor.primary} bg-${bgColor.primary} border border-${borderColor.primary} float-right button-regenerate rounded-md p-1 `}
           disabled={userInput.trim().length < 3}
           onClick={handleRegenerate}
           style={{ zIndex: 1 }}
@@ -65,10 +65,10 @@ const AccessibleChat: React.FC = () => {
             maxRows={10}
             onChange={(e) => setUserInput(e.target.value)}
             placeholder="Send a message..."
-            className={`text-${textColorPrimary} w-full resize-none border-0 bg-transparent py-[10px] pr-10 focus:ring-0 focus-visible:ring-0 dark:bg-transparent md:py-4 md:pr-12 pl-3 md:pl-4 overflow-y-auto`}
+            className={`text-${textColor.primary} w-full resize-none border-0 bg-transparent py-[10px] pr-10 focus:ring-0 focus-visible:ring-0 dark:bg-transparent md:py-4 md:pr-12 pl-3 md:pl-4 overflow-y-auto`}
           />
           <Button
-            className={`text-${textColorPrimary} bg-${bgColor.secondary} button-send absolute p-1 rounded-md md:bottom-3 md:p-2 md:right-3 dark:hover:bg-gray-900 dark:disabled:hover:bg-transparent right-2 transition-colors disabled:opacity-40`}
+            className={`text-${textColor.primary} bg-${bgColor.secondary} button-send absolute p-1 rounded-md md:bottom-3 md:p-2 md:right-3 dark:hover:bg-gray-900 dark:disabled:hover:bg-transparent right-2 transition-colors disabled:opacity-40`}
             disabled={userInput.trim().length < 3}
             type="submit"
             style={{ zIndex: 1 }}
