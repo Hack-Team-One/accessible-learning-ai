eb548ae1a11a405d54ecb2701ca13ea4ed07afa3
burtrico <burtrico@gmail.com>
2023-10-02 16:40:24 -0500

    added theme module augmentation to allow theme custom colors

--- theme.d.ts
+++ theme.d.ts
@@ -2,6 +2,40 @@ import '@mui/material/styles';
 
 declare module '@mui/material/styles' {
   interface Palette {
-    custom?: PaletteOptions;
+    custom: {
+      text: {
+        tertiary: string;
+        titleSecondary: string;
+      };
+      background: {
+        tertiary: string;
+        buttonPrimary: string;
+        buttonSecondary: string;
+      };
+      border: {
+        primary: string;
+        secondary: string;
+        tertiary: string;
+      };
+    };
+  }
+
+  interface PaletteOptions {
+    custom?: {
+      text?: {
+        tertiary?: string;
+        titleSecondary?: string;
+      };
+      background?: {
+        tertiary?: string;
+        buttonPrimary?: string;
+        buttonSecondary?: string;
+      };
+      border?: {
+        primary?: string;
+        secondary?: string;
+        tertiary?: string;
+      };
+    };
   }
 }
