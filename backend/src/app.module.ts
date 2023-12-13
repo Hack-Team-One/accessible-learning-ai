c82411c2bffcc4e54e460aefea08ef1bda2f1b97
burtrico <burtrico@gmail.com>
2023-12-12 21:47:51 -0600

    added rate limiter to localAuthGuard

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -5,6 +5,7 @@ import { UsersModule } from './modules/users/users.module';
 import { AuthModule } from './modules/auth/auth.module';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { RateLimiterModule } from 'nestjs-rate-limiter';
 
 @Module({
   imports: [
@@ -12,6 +13,12 @@ import { AppService } from './app.service';
       ...AppDataSource.options,
     }),
     AuthModule,
+    RateLimiterModule.register({
+      // You can customize these options
+      type: 'Memory',
+      points: 5, // Number of points
+      duration: 60, // Duration in seconds
+    }),
     UsersModule,
   ],
   controllers: [AppController],
--- backend/src/modules/auth/localAuth.guard.ts
+++ backend/src/modules/auth/localAuth.guard.ts
@@ -1,7 +1,31 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, ExecutionContext } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
+import {
+  RateLimiterGuard,
+  RateLimiterModuleAsyncOptions,
+} from 'nestjs-rate-limiter';
 
 @Injectable()
 export class LocalAuthGuard extends AuthGuard('local') {
-  // You can add additional methods or properties if needed
+  private rateLimiterGuard: RateLimiterGuard;
+
+  constructor() {
+    super();
+    const rateLimiterOptions: RateLimiterModuleAsyncOptions = {
+      // Specify your rate limiting options here
+      for: 'Express',
+      type: 'Memory',
+      points: 5, // Number of points
+      duration: 60, // Per second(s)
+      // Other options as required
+    };
+    this.rateLimiterGuard = new RateLimiterGuard(rateLimiterOptions);
+  }
+
+  async canActivate(context: ExecutionContext): Promise<boolean> {
+    await this.rateLimiterGuard.canActivate(context); // Apply rate limiting
+    return super.canActivate(context); // Continue with the usual guard checks
+  }
+
+  // Additional methods or properties can be added as needed
 }
