5babec5e13fa52108d22a0f0bde1fce03cda75a2
burtrico <burtrico@gmail.com>
2023-12-21 16:27:58 -0600

    updated login and register pages to validate email and password input fields

--- interface/src/pages/_app.tsx
+++ interface/src/pages/_app.tsx
@@ -5,6 +5,14 @@ import type { AppProps } from 'next/app';
 
 function MyApp({ Component, pageProps }: AppProps) {
 
+  // const isReady = useMemo(() => typeof window !== 'undefined', []);
+	// const currentTheme = useRecoilValue(currentThemeState);
+	// const theme = useMemo(() => themes[currentTheme], [currentTheme]);
+	// // @ts-ignore palette options
+	// const muiTheme = useMemo(() => createTheme(getDesignTokens(currentTheme)), [currentTheme]);
+
+  // theme={currentTheme === 'dark' ? darkTheme() : lightTheme()} 
+
   return (
     <RecoilRoot>
         <Component {...pageProps} />
--- interface/src/pages/login.tsx
+++ interface/src/pages/login.tsx
@@ -1,21 +1,71 @@
 // pages/login.tsx
 import React, { useState } from 'react';
 import axios from 'axios';
-import { Button, TextField, Container, Typography } from '@mui/material';
+import { Button, TextField, Container, Typography, IconButton, InputAdornment } from '@mui/material';
+import VisibilityIcon from '@mui/icons-material/Visibility';
+import VisibilityOffIcon from '@mui/icons-material/VisibilityOff';
+import Joi from 'joi';
+import Link from 'next/link';
 
 export default function Login() {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+  const [showPassword, setShowPassword] = useState(false);
+  const [emailError, setEmailError] = useState(false);
+
+  const schema = Joi.object({
+    email: Joi.string().email({ tlds: { allow: false } }).required(),
+  });
 
   const handleLogin = async () => {
-    // Implement your login logic here
+    const { error } = schema.validate({ email });
+    setEmailError(!!error);
+
+    if (!error) {
+      // API call to login endpoint
+    }
   };
 
   return (
     <Container maxWidth="sm">
       <Typography variant="h4" gutterBottom>Login</Typography>
-      {/* Email and Password TextFields */}
-      {/* Login Button */}
+      <TextField
+        error={emailError}
+        helperText={emailError ? "Invalid email address" : ""}
+        label="Email"
+        variant="outlined"
+        fullWidth
+        margin="normal"
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+      />
+      <TextField
+        label="Password"
+        type={showPassword ? "text" : "password"}
+        variant="outlined"
+        fullWidth
+        margin="normal"
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+        InputProps={{
+          endAdornment: (
+            <InputAdornment position="end">
+              <IconButton
+                onClick={() => setShowPassword(!showPassword)}
+                edge="end"
+              >
+                {showPassword ? <VisibilityIcon /> : <VisibilityOffIcon />}
+              </IconButton>
+            </InputAdornment>
+          ),
+        }}
+      />
+      <Button variant="contained" color="primary" onClick={handleLogin}>
+        Login
+      </Button>
+      <Typography variant="body1" marginTop={2}>
+        If you do not have an account yet, please <Link href="/register"><a>Sign Up</a></Link>.
+      </Typography>
     </Container>
   );
 }
