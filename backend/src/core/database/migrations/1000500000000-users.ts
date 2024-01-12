1cade4d18644eef7575b1f3a69ed8515d4867aba
burtrico <burtrico@gmail.com>
2024-01-11 20:46:12 -0600

    completed users table migration file

--- backend/src/core/database/migrations/1000500000000-users.ts
+++ backend/src/core/database/migrations/1000500000000-users.ts
@@ -3,14 +3,25 @@ import { MigrationInterface, QueryRunner } from 'typeorm';
 export class users1000500000000 implements MigrationInterface {
   name = 'users1000500000000';
 
-  // eslint-disable-next-line class-methods-use-this
   public async up(queryRunner: QueryRunner): Promise<void> {
-    await queryRunner.query(
-      'CREATE TABLE "users" ("id" SERIAL NOT NULL, "firstName" character varying(100) NOT NULL, "lastName" character varying(100) NOT NULL, "email" character varying NOT NULL, "password" character varying(100) NOT NULL, "createdAt" TIMESTAMP NOT NULL DEFAULT now(), CONSTRAINT "UQ_users_email" UNIQUE ("email"), CONSTRAINT "PK_users_id" PRIMARY KEY ("id"))',
-    );
+    await queryRunner.query(`
+      CREATE TABLE "users" (
+        "id" SERIAL NOT NULL,
+        "email" character varying(100) NOT NULL,
+        "password" character varying(100) NOT NULL,
+        "firstName" character varying(100),
+        "lastName" character varying(100),
+        "emailVerificationCode" character varying(50),
+        "emailVerified" boolean NOT NULL DEFAULT false,
+        "createdAt" TIMESTAMP NOT NULL DEFAULT now(),
+        "updatedAt" TIMESTAMP NOT NULL DEFAULT now(),
+        "deletedAt" TIMESTAMP,
+        CONSTRAINT "PK_users_id" PRIMARY KEY ("id"),
+        CONSTRAINT "UQ_users_email" UNIQUE ("email")
+      )
+    `);
   }
 
-  // eslint-disable-next-line class-methods-use-this
   public async down(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.query('DROP TABLE "users"');
   }
